d5e28f4c31ec0cd22960c51962a53a3f0620a2a5
Vishal Mishra <vishalm434@gmail.com>
2026-02-26 20:22:56 +0000

    Map chapter

---
+++ dictionary/dictionary.go
@@ -0,0 +1,67 @@
+package dictionary
+
+
+type Dictionary map[string]string
+
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot perform operation on word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+func (d Dictionary) Search(word string) (string, error){
+	value, ok := d[word]
+	if ok {
+		return value, nil
+	}
+	return "", ErrNotFound
+}
+
+func (d Dictionary) Add(key, value string) error {
+	_ ,err := d.Search(key)
+	switch err {
+	case ErrNotFound:
+		d[key] = value
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		delete(d, word)
+	default:
+		return err
+	}
+
+	return nil
+}
