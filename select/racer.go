73a4f377743b0983cf77ceccd04a76a6c116d064
Vishal Mishra <vishalm434@gmail.com>
2026-03-01 17:29:05 +0000

    Complete select chapter

--- select/racer.go
+++ select/racer.go
@@ -3,35 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
 	"time"
 )
 
-func Racer(urls  ...string) string {
-	loadTimes := make([]int64, len(urls))
-	var wg sync.WaitGroup
-	for i, url := range urls {
-		wg.Add(1)
-		go func (i int) {
-		 	getLoadTime(url, loadTimes, i)
-			wg.Done()
-		}(i)
-	}
-	wg.Wait()
-	idx, minLoadTime := 0, int64((1 << 63) - 1)
-	for i, time := range loadTimes {
-		if time <= minLoadTime {
-			fmt.Print(minLoadTime)
-			minLoadTime = time
-			idx = i
-		}
-	}
-	return urls[idx]
+var TimeOutError = fmt.Errorf("Url fetching took longer than 10 sec")
+var tenSecondTimeOut = time.Second*10
+
+func Racer(a, b string) (string, error) {
+	return configurableRacer(a, b, tenSecondTimeOut)
 }
 
-func getLoadTime(url string, loadTimes []int64, idx int) {
-	start := time.Now()
-	http.Get(url)
-	duration := time.Since(start)
-	loadTimes[idx] = int64(duration)
+func configurableRacer(a, b string, timeout time.Duration) (string, error) {
+	select {
+	case <- ping(a):
+		return a, nil
+	case <- ping(b):
+		return b, nil
+	case <- time.After(timeout):
+	   return "", TimeOutError
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
